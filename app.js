30b0c6f3bd74d7bb402629bc9e37b7378b2de819
Gaurav Singh <gauravnumber@gmail.com>
2021-07-22 11:26:03 +0530

    Github Action not ok

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.send('not ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('4') // change this string to ensure a new version deployed
+  res.send('5')
 })
 
 
